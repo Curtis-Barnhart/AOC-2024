cb9400dc38b9775d0258970edbb6c5f0651bb66d
Curtis Barnhart <cbarnhart@westmont.edu>
2024-12-08 08:17:12 -0800

    Added Curtis solutions to part day 2.

---
+++ 02/curtis.part1.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def is_safe(numbers):
+    up_or_down = None
+
+    for a, b in zip(numbers, numbers[1:]):
+        if up_or_down is None:
+            up_or_down = b - a
+            if abs(up_or_down) > 3 or up_or_down == 0:
+                return False
+        else:
+            if up_or_down < 0 and b >= a \
+            or up_or_down > 0 and b <= a \
+            or a - b == 0 \
+            or abs(a - b) > 3:
+                return False
+    return True
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "r") as file:
+        lines = [
+            [int(n) for n in line.split(" ")]
+            for line in file.readlines()
+        ]
+
+    print("Solution: {:d}!".format(sum((is_safe(line) for line in lines))))
+
---
+++ 02/curtis.part2.py
@@ -0,0 +1,44 @@
+import sys
+
+
+def remove_at(list_: list[int], index: int):
+    c = list_.copy()
+    c.pop(index)
+    return c
+
+
+def is_safe(numbers):
+    up_or_down = None
+
+    for a, b in zip(numbers, numbers[1:]):
+        if up_or_down is None:
+            up_or_down = b - a
+            if abs(up_or_down) > 3 or up_or_down == 0:
+                return False
+        else:
+            if up_or_down < 0 and b >= a \
+            or up_or_down > 0 and b <= a \
+            or a - b == 0 \
+            or abs(a - b) > 3:
+                return False
+    return True
+
+
+def is_safe_any(numbers):
+    if is_safe(numbers):
+        return True
+    for i in range(len(numbers)):
+        if is_safe(remove_at(numbers, i)):
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "r") as file:
+        lines = [
+            [int(x) for x in line.split(" ")]
+            for line in file.readlines()
+        ]
+
+    print("Solution: {:d}!".format(sum((is_safe_any(line) for line in lines))))
+
